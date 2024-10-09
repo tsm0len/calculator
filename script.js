72a582590e5d63673990a8f429e82097e059519f
sm0L <122048823+tsm0len@users.noreply.github.com>
2024-10-09 11:43:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let input = "";
+let output = 0;
+const maxnumbers = 6;
+function button(id) {
+    if (input.length<maxnumbers) {
+        input = input+id;
+        console.log(input);
+        document.getElementById("screen").textContent = input;
+    }
+}
+function calc() {
+    output = eval(input);
+    console.log(output);
+    document.getElementById("screen").textContent = output;
+}
+function clear() {
+    //input = "";
+    //output = 0;
+    console.log("cleared");
+    //document.getElementById("screen").textContent = "0";
+}
